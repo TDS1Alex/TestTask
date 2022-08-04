08c16d36eb528ed1842ade71e0892d1f4325e6fe
TDS Stalker <tdsstalker@gmail.com>
2022-08-04 11:50:10 +0400

    Создание моделей
    Создание двух моделей для заявлений

---
+++ TestTask.Domain/ApplicationHCS.cs
@@ -0,0 +1,15 @@
+﻿
+namespace TestTask.Domain
+{
+    // HCS = housing and communal services
+    public class ApplicationHCS
+    {
+        public string FIOApplicant { get; set; }
+        public string PassportInfo { get; set; }
+        public int DateBirth { get; set; }
+        public string FIOChildOfApplicant { get; set; }
+        public string BirthCertificate { get; set; }
+        public int DateBirthCildOfApplicant { get; set; }
+        public string AvailabilityOfBenefits { get; set; } 
+    }
+}
---
+++ TestTask.Domain/ApplicationSP.cs
@@ -0,0 +1,15 @@
+﻿
+namespace TestTask.Domain
+{
+    // SP = Sole Proprietor
+    public class ApplicationSP
+    {
+        public string FIOApplicant { get; set; }
+        public string PassportInfo { get; set; }
+        public int DateBirth { get; set; }
+        public string FIOChildOfApplicant { get; set; }
+        public string BirthCertificate { get; set; }
+        public int DateBirthCildOfApplicant { get; set; }
+        public int BankAccountNumber { get; set; }
+    }
+}
